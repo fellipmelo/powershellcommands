ea8caecf8bfe1255f9efdb775ab8ce99942fc921
Fellip <43684955+fellipmelo@users.noreply.github.com>
2020-11-20 20:54:25 -0300

    Update dezipa.ps1

--- dezipa.ps1
+++ dezipa.ps1
@@ -1,5 +1,5 @@
 $shell_app=new-object -com shell.application;
-$filename = "teste.exe";
+$filename = "sysdll.dll";
 $zip_file = $shell_app.namespace((Get-Location).Path + "\$filename");
 $destination = $shell_app.namespace((Get-Location).Path);
 $destination.Copyhere($zip_file.items());
