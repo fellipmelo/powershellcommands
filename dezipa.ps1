45e648c907c4ddaefc69329a206062c640bea61a
Fellip <43684955+fellipmelo@users.noreply.github.com>
2020-11-20 19:59:36 -0300

    Add files via upload

---
+++ dezipa.ps1
@@ -0,0 +1,5 @@
+$shell_app=new-object -com shell.application;
+$filename = "teste.exe";
+$zip_file = $shell_app.namespace((Get-Location).Path + "\$filename");
+$destination = $shell_app.namespace((Get-Location).Path);
+$destination.Copyhere($zip_file.items());
